7a9bb9bf859a1f0117785b33f5852d9f3450eb51
Colin-Fredericks <colin.fredericks@gmail.com>
2017-01-03 19:34:03 -0500

    Revert "Hopefully fixing reroll-and-keep bug"
    This reverts commit 601e018de235a047828146789c613931a96a6d58.

--- Ex3DiceRoll.js
+++ Ex3DiceRoll.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
 
         // Make the array that has all our raw dice roll results.
         for(var i = 0; i < dicePool; i++){
-            var thisDie = rollDie(options, targetNumber, doublesArray);
+            var thisDie = rollDie(options);
             resultsArray.push(thisDie);
             if(thisDie >= targetNumber){successes += 1;}
             if(thisDie == 1 && onesSubtract) {successes -= 1;}
@@ -139,12 +139,8 @@ $(document).ready(function(){
     });
 
     // Returns a single die result.
-    function rollDie(options, targetNumber, doublesArray){
+    function rollDie(options){
         var result = Math.ceil(Math.random() * 10);
-
-        // Keep Successes seems to be a general rule.
-        if(result >= targetNumber){successes += 1;}
-        if(doublesArray.indexOf(thisDie) != -1){successes += 1;}
         
         // Check for rerolls. Two different types.
         if(options.rerollForever == false){
@@ -158,7 +154,7 @@ $(document).ready(function(){
         else{
             // Reroll forever? Uses recursion.
             if(options.rerollArray.indexOf(result) != -1){
-                return rollDie(options, targetNumber, doublesArray);
+                return rollDie(options);
             }else{
                 return result;
             }
